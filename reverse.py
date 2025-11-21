4e141e5df5bc3c2f0856aa149b12d562e569f36b
delerictofparadise-spec <delerictofparadise@gmail.com>
2025-11-21 13:37:34 +0300

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,28 @@
+def reverse_bykv(text: str) -> str:
+    if not isinstance(text,str):
+        raise ValueError('text must be a string')
+
+
+    s = [x for x in text if x.isalpha()]
+    s.reverse()
+
+    result = []
+    index = 0
+
+    for i in range(len(text)):
+        inx = text[i]
+        if inx.isalpha():
+            result.append(s[index])
+            index += 1
+        else:
+            result.append(inx)
+
+    return ''.join(result)
+
+
+if __name__ == '__main__':
+    test1 = 'syka12dadasdsgergr4'
+    test2 = 'govonzalupapen1s'
+
+    print(reverse_bykv(test1))
+    print(reverse_bykv(test2))
